d7ebb07fc74979733e47f6567ece5879db1cf40a
Uchenna Ogbodo <Uchenna.Ogbodo@Interswitchng.com>
2026-02-27 20:11:14 +0100

    update

--- src/app/(dashboard)/inventory/feedback.tsx
+++ src/app/(dashboard)/inventory/feedback.tsx
@@ -1,14 +1,14 @@
-import { Response } from "@/types/reponse";
-import { AlertTriangle, CheckCircle } from "lucide-react";
+// import { Response } from "@/types/reponse";
+// import { AlertTriangle, CheckCircle } from "lucide-react";
 
-export const FeedbackMessage: React.FC<Response> = ({ success, message }) => (
-    <>
-    {
-        console.log("success | message", success, message)
-    }
-    <div className={`p-4 rounded-lg flex items-center space-x-3 ${success ? 'bg-green-100 text-green-700' : 'bg-red-100 text-red-700'} shadow-md mb-4`}>
-        {success ? <CheckCircle className="w-5 h-5" /> : <AlertTriangle className="w-5 h-5" />}
-        <p className="font-medium text-sm">{message}</p>
-    </div>
-    </>
-);
+// export const FeedbackMessage: React.FC<Response> = ({ success, message }) => (
+//     <>
+//     {
+//         console.log("success | message", success, message)
+//     }
+//     <div className={`p-4 rounded-lg flex items-center space-x-3 ${success ? 'bg-green-100 text-green-700' : 'bg-red-100 text-red-700'} shadow-md mb-4`}>
+//         {success ? <CheckCircle className="w-5 h-5" /> : <AlertTriangle className="w-5 h-5" />}
+//         <p className="font-medium text-sm">{message}</p>
+//     </div>
+//     </>
+// );
