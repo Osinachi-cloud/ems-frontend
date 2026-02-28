fa959df1b8c38fda0da5d82a60325ac5d64aead0
Uchenna Ogbodo <Uchenna.Ogbodo@Interswitchng.com>
2026-02-28 07:04:05 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Header } from "@/components/Header/Header";
 
 import Image from "next/image";
-import { useEffect } from "react";
 import { HomePage } from "./homePage/HomePage";
 
 export default function Home() {
